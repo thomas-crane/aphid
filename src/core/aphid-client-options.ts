7fa9df3ccac26df120b28b5d35c7bfe8f7206400
thomas-crane <thomasgcrane@gmail.com>
2019-03-23 20:30:06 +1100

    feat(core): add aphid client
    This commit adds a basic aphid client. So far, it can handle messages
    and dispatch commands to the relevant command handlers.

---
+++ src/core/aphid-client-options.ts
@@ -0,0 +1,13 @@
+/**
+ * Configuration values for the @see AphidClient class.
+ */
+export interface AphidClientOptions {
+  /**
+   * The prefix which will trigger commands for this Aphid bot.
+   */
+  prefix: string;
+  /**
+   * The author of this Aphid bot.
+   */
+  author: string;
+}
---
+++ src/core/aphid-client.ts
@@ -0,0 +1,55 @@
+import { Client, ClientOptions, Message } from 'discord.js';
+import * as commands from '../commands';
+import log from '../log';
+import { Type } from '../models/type';
+import * as modules from '../modules';
+import { AphidClientOptions } from './aphid-client-options';
+
+/**
+ * A discord bot client.
+ */
+export class AphidClient extends Client {
+
+  constructor(private aphidOptions: AphidClientOptions, clientOptions?: ClientOptions) {
+    super(clientOptions);
+    this.on('message', this.handleMessage.bind(this));
+  }
+
+  /**
+   * Loads the module provided.
+   * @param module The module to load.
+   */
+  loadModule(module: Type<any>): void {
+    log.info('AphidClient', `Loading ${module.name}`);
+    const info = modules.loader.getInfo(module.name);
+    if (info) {
+      log.success('AphidClient', `Loaded module: ${info.name}!`);
+    } else {
+      log.error('AphidClient', `Failed to load module ${module.name}`);
+    }
+  }
+
+  /**
+   * Handles a message and invokes the relevant command if there is one.
+   * @param message The message to handle.
+   */
+  private handleMessage(message: Message) {
+    // make sure it's not a bot message.
+    if (message.author.bot) {
+      return;
+    }
+    // make sure the message starts with the prefix.
+    if (message.content.slice(0, this.aphidOptions.prefix.length) !== this.aphidOptions.prefix) {
+      return;
+    }
+    // get the command part.
+    const cmd = message.content.split(' ')[0].slice(this.aphidOptions.prefix.length);
+
+    // check if there are any triggers for this command.
+    if (!commands.loader.hasTrigger(cmd)) {
+      return;
+    }
+
+    commands.loader.invoke(cmd, this, message);
+  }
+}
